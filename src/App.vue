7a14fd30b2342307ddd489175cb98518e711057a
gusheng <804657288@qq.com>
2018-09-21 09:46:11 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    {{test}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      test:"aaa"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  color:red;
+}
+</style>
